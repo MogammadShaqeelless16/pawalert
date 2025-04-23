16d7f86a0e35bb0ec29550a557676e7f4c0e1b18
shaqeelless4 <shaqeelless4@gmail.com>
2025-04-23 23:45:41 +0000

    update changes.

--- obj/Debug/net8.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1f4331d5d35506c32428561e01ea561650946f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9b3385452be450bef328e780df752d2d12375f")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
