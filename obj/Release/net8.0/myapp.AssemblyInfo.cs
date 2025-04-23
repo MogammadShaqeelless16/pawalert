644d1b0b7cb24427062d5a2ebf49b6fcb923aff5
shaqeelless4 <shaqeelless4@gmail.com>
2025-04-23 23:02:22 +0000

    upDATE CHANGES

--- obj/Release/net8.0/myapp.AssemblyInfo.cs
+++ obj/Release/net8.0/myapp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1f4331d5d35506c32428561e01ea561650946f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
