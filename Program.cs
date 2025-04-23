448dc1d31d0c894c596a7d28bf13d67f3c1ef6bb
shaqeelless4 <shaqeelless4@gmail.com>
2025-04-23 23:34:40 +0000

    update program.

--- Program.cs
+++ Program.cs
@@ -8,19 +8,25 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
+// 👇 Add this if you're hosting under a sub-path like "/pawalert"
+app.UsePathBase("/pawalert");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+app.UseRouting();
 app.UseAntiforgery();
 
+// 👇 Fallback route for deep linking
+app.MapFallbackToPage("/_Host");
+
+// Razor components mapping
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
